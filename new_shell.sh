cd62519a2ab007e53b050a52dc2c7d3f598efd82
root <root@new-multiscanner.(none)>
2015-03-31 14:37:36 -0700

    pathing fixed.

--- new_shell.sh
+++ new_shell.sh
@@ -13,14 +13,14 @@ yum -y install wget
 echo "Installing oh-my-zsh..."
 wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 echo "Downloading and replacing ~/.vimrc..."
-wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.vimrc -O ~/.vimrc
+wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.vimrc -O /home/$1/.vimrc
 echo "Downloading and replacing ~/.zshrc..."
-wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.zshrc -O ~/.zshrc
+wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.zshrc -O /home/$1/.zshrc
 echo "Creating vim colors dir and downloading vim colors..."
-mkdir -p ~/.vim/colors
-wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.vimrc -O ~/.vim/colors/wombat256mod.vim
+mkdir -p /home/$1/.vim/colors
+wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.vimrc -O /home/$1/.vim/colors/wombat256mod.vim
 echo "Adding vi -> vim alias..."
-echo "alias vi='/usr/bin/vim'" >> ~/.zshrc
+echo "alias vi='/usr/bin/vim'" >> /home/$1/.zshrc
 echo "Moving .oh-my-zsh and changing shell..."
 cp -r /root/.oh-my-zsh /home/$1/.oh-my-zsh
 chsh -s `which zsh` $1
