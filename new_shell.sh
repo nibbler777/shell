bee91fa0c3a6915f49f23d9fe28f4accba65764b
root <root@new-multiscanner.(none)>
2015-03-31 14:23:53 -0700

    wget fixed

--- new_shell.sh
+++ new_shell.sh
@@ -11,14 +11,14 @@ yum -y install zsh
 echo "Installing wget..."
 yum -y install wget
 echo "Installing oh-my-zsh..."
-wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 echo "Downloading and replacing ~/.vimrc..."
-wget https://raw.githubusercontent.com/nibbler777/shell/master/.vimrc -O ~/.vimrc
+wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.vimrc -O ~/.vimrc
 echo "Downloading and replacing ~/.zshrc..."
-wget https://raw.githubusercontent.com/nibbler777/shell/master/.zshrc -O ~/.zshrc
+wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.zshrc -O ~/.zshrc
 echo "Creating vim colors dir and downloading vim colors..."
 mkdir -p ~/.vim/colors
-wget https://raw.githubusercontent.com/nibbler777/shell/master/.vimrc -O ~/.vim/colors/wombat256mod.vim
+wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.vimrc -O ~/.vim/colors/wombat256mod.vim
 echo "Adding vi -> vim alias..."
 echo "alias vi='/usr/bin/vim'" >> ~/.zshrc
 echo "Moving .oh-my-zsh and changing shell..."
