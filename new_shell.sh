0297f752a090037b3728b884f13d82866e7f4469
root <root@new-multiscanner.(none)>
2015-03-31 15:27:36 -0700

    arg checker and .zshrc export fixed.

--- new_shell.sh
+++ new_shell.sh
@@ -2,6 +2,11 @@
 
 #Installs and prepares zsh and vim
 
+if [ $# -eq 0 ]
+    then
+        echo "No arguments supplied"
+        exit 1
+fi
 echo "Installing curl-devel..."
 yum -y install curl-devel
 echo "Installing git..."
@@ -21,6 +26,8 @@ mkdir -p /home/$1/.vim/colors
 wget --no-check-certificate https://raw.githubusercontent.com/nibbler777/shell/master/.vimrc -O /home/$1/.vim/colors/wombat256mod.vim
 echo "Adding vi -> vim alias..."
 echo "alias vi='/usr/bin/vim'" >> /home/$1/.zshrc
+echo "Fixing home directory for oh-my-zsh directory..."
+sed -i -e 's/change_user/jseals/g' /home/$1/.zshrc
 echo "Moving .oh-my-zsh and changing shell..."
 cp -r /root/.oh-my-zsh /home/$1/.oh-my-zsh
 chsh -s `which zsh` $1
